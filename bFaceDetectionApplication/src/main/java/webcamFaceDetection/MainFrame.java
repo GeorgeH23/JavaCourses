a7f6ff4bc845278613a8ed22e6c8f6de1cead418
George <gheorghita.harpa0423@yahoo.com>
2020-04-05 19:59:58 +0100

    Implemented quick sort algorithm in dPracticalDataStructuresAndAlgorithmsInJava module.

--- bFaceDetectionApplication/src/main/java/webcamFaceDetection/MainFrame.java
+++ bFaceDetectionApplication/src/main/java/webcamFaceDetection/MainFrame.java
@@ -1,7 +1,6 @@
 package webcamFaceDetection;
 
 import javax.swing.JFrame;
-import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.videoio.VideoCapture;
 
--- dPracticalDataStructuresAndAlgorithmsInJava/src/main/java/Algorithms/gQuickSort/App.java
+++ dPracticalDataStructuresAndAlgorithmsInJava/src/main/java/Algorithms/gQuickSort/App.java
@@ -1,6 +1,10 @@
 package Algorithms.gQuickSort;
 
+import java.util.Arrays;
+
 public class App {
+
+    private static int callCount = 0;
 
     public static void main(String[] args) {
 
@@ -9,5 +13,63 @@ public class App {
          *                             -best case scenario is Ω(n log(n))
          *                             -average case scenario is θ(n log(n))
          * */
+
+        int[] inputArray = {12, 81, 74, 43, 1098, 0, 8, 92, 17, 754, 912, 0, 6, 4};
+        quickSort(inputArray, 0, inputArray.length-1);
+
+        System.out.println(Arrays.toString(inputArray));
+        System.out.println("Number of times quickSort() was called: " + callCount);
+    }
+
+    private static void quickSort(int[] inputArray, int start, int end) {
+        callCount++;
+        if (start < end) {
+            int partitionPoint = partition(inputArray,start,end); /* index position of the correctly placed
+                                                                     pivot value in the array*/
+            quickSort(inputArray,start,partitionPoint-1); // sorts the left half of the range
+            quickSort(inputArray,partitionPoint+1,end); // sorts the right half of the range
+        }
+
+    }
+
+    private static int partition(int[] inputArray, int start, int end) {
+
+        int pivot = inputArray[end];
+        int i = start - 1;
+        int temp;
+
+        for (int j = start; j <= end-1; j++) {
+            if (inputArray[j] <= pivot) {
+                i++;
+                // swapping inputArray[i] with inputArray[j]
+                temp = inputArray[i];
+                inputArray[i] = inputArray[j];
+                inputArray[j] = temp;
+            }
+        }
+
+        // put the pivot value in the correct slot
+        temp = inputArray[i+1];
+        inputArray[i+1] = pivot; // here the pivot is placed in the correct slot of the array
+        inputArray[end] = temp;
+
+        return i+1;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
